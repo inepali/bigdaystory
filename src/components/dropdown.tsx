eef2bee1fef4ecbcc3b52c287693c0832d10a577
inepali <sanjayaghimire@gmail.com>
2024-11-20 17:34:31 -0500

    import fix

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -3,7 +3,7 @@
 import * as Headless from '@headlessui/react';
 import clsx from 'clsx';
 import type React from 'react';
-import { Button } from './button';
+import { Button } from './Button';
 import { Link } from './Link';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
